9843a4e53dbbab39c0d45783d21aeecaed383b24
georgemcclure1295-maker <georgemcclure1295@gmail.com>
2026-07-23 17:14:13 +0100

    Delete gotr-rune-tracker directory

--- gotr-rune-tracker/src/main/java/com/gotrrunetracker/GotrRuneTrackerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.gotrrunetracker;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-
-@ConfigGroup("gotrrunetracker")
-public interface GotrRuneTrackerConfig extends Config
-{
-}
